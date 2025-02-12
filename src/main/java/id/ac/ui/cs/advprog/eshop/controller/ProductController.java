52c6a287003304d6bc0957f4efaf87b818b3a15e
Adam <adamsembiring01@gmail.com>
2025-02-12 22:00:25 +0700

    feat: create controller

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -0,0 +1,44 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/")
+    public String getHome(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String createProduct(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@ModelAttribute Product product, Model model) {
+        productService.create(product);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String listProduct(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "productList";
+    }
+}
