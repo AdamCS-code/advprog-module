f2cc6be3a00caa0a6529761136d5c3ea0a493d82
Adam <adamsembiring01@gmail.com>
2025-02-17 23:53:44 +0700

    Unnecessary modifier 'public' on method 'findById': the method is declared in an interface type

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface ProductService {
     Product create(Product product);
     public List<Product> findAll();
-    public Product findById(String productId);
+    Product findById(String productId);
     public Product edit(Product product);
     public void delete(String productId);
 }
